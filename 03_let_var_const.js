9c16e926a0789368823be23d3ac9341234dbf732
Akash Kushwaha <akashkushwaha76986.com>
2024-05-29 20:04:38 +0530

    var, let and const

---
+++ 03_let_var_const.js
@@ -0,0 +1,15 @@
+console.log("Var, let and const")
+// var a = 45;
+// var a = "p";
+let b = "Akash";
+const author = "Akash"
+author = 5 // Throws an error because constant cannot be changed
+b = 4
+const akash = 0;
+let c = null;
+let d = undefined;
+{
+    let b = "this"
+    console.log(b)
+}
+console.log(b)
