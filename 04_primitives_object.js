ba1bdc337ae4cef6a639e6f1ab48953a240afbe9
Akash Kushwaha <akashkushwaha76986.com>
2024-05-29 20:26:33 +0530

    Primitives and Objects

---
+++ 04_primitives_object.js
@@ -0,0 +1,19 @@
+// nn bb ss u - Primitives in JS
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("543") + BigInt("3")
+let e = "Akash"
+let f = Symbol("I am a nice symbol")
+let g 
+console.log(a, b, c, d, e, f, g)
+console.log(typeof c)
+
+// Non Primitive Data Type - Objects in JS
+const item = {
+    "Item0": true,
+    "Item1": false,
+    "Item2": 45,
+    "Item3": undefined
+}
+console.log(item["Item1"])
