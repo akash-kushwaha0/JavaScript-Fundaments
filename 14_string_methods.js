a5b49ef2bc1d660e24d58e4bb3539c244cbaea50
Akash Kushwaha <akashkushwaha76986.com>
2024-05-31 22:36:49 +0530

    String methods

---
+++ 14_string_methods.js
@@ -0,0 +1,21 @@
+let name = "Akash"
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+
+// console.log(name.toLowerCase())
+
+// console.log(name.slice(2,4))
+// console.log(name.slice(2))
+
+// console.log(name.replace("Ak", "Pr"))
+
+let friend = "Naman"
+console.log(name.concat(" is a friend of " , friend, " OK "))
+
+let friend2 = "    Meena     "
+console.log(friend2)
+console.log(friend2.trim())
