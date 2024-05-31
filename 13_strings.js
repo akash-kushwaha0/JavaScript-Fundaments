09389aa6a9719b604d9f947486ac5b7749c33a68
Akash Kushwaha <akashkushwaha76986.com>
2024-05-31 22:17:50 +0530

    Strings

---
+++ 13_strings.js
@@ -0,0 +1,22 @@
+// Double quote string
+let name = "Akash"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+
+// Single quote string
+let friend = 'aakash'
+console.log(friend)
+
+// Template literals
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+// let fruit = 'Banana\'s'
+// console.log(fruit)
+let fruit = "Banana\"s"
+console.log(fruit)
