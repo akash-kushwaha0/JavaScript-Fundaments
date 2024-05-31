02047ec1550592af4c134c3bfcc41538413d4043
Akash Kushwaha <akashkushwaha76986.com>
2024-05-31 18:56:14 +0530

    While loop

---
+++ 10_while_loop.js
@@ -0,0 +1,20 @@
+// while loop
+const prompt=require("prompt-sync")({signit:true});
+// let n = prompt("Enter the value of n: ")
+// n = Number.parseInt(n)
+
+// let i = 0;
+// while(i<n){
+//     console.log(i)
+//     i++;
+// }
+
+// do while loop
+let n = prompt("Enter the value of n: ")
+n = Number.parseInt(n)
+
+let i = 0;
+do{
+    console.log(i)
+    i++;
+}while(i<n)
