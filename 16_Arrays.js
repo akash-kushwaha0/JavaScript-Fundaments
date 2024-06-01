a2895a8e0bf0134061bebb511eccc753ac7f5598
Akash Kushwaha <akashkushwaha76986.com>
2024-06-01 17:19:42 +0530

    Arrays

---
+++ 16_Arrays.js
@@ -0,0 +1,13 @@
+let marks_class_12 = [95, 89, 78, 90, false, "Not present"]
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6])
+console.log("The length of marks_class_12", marks_class_12.length)
+marks_class_12[6] = 69 // Adding a new value to the array
+marks_class_12[0] = 98 // Changing the value of an array
+console.log(marks_class_12)
+console.log(typeof marks_class_12)
